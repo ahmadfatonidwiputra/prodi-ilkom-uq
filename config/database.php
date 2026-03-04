b3209e42fcfe68a3fdb681a5affd5de3ad83c42b
ahmadfatoni_dp <ahmadfatonidwiputra@Ahmads-MacBook-Air.local>
2026-03-04 11:34:11 +0800

    try migrate

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -44,12 +44,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('MYSQL_URL', env('DATABASE_URL', env('DB_URL'))),
+            'host' => env('MYSQLHOST', env('DB_HOST', '127.0.0.1')),
+            'port' => env('MYSQLPORT', env('DB_PORT', '3306')),
+            'database' => env('MYSQLDATABASE', env('DB_DATABASE', 'laravel')),
+            'username' => env('MYSQLUSER', env('DB_USERNAME', 'root')),
+            'password' => env('MYSQLPASSWORD', env('DB_PASSWORD', '')),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
@@ -64,12 +64,12 @@ return [
 
         'mariadb' => [
             'driver' => 'mariadb',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('MYSQL_URL', env('DATABASE_URL', env('DB_URL'))),
+            'host' => env('MYSQLHOST', env('DB_HOST', '127.0.0.1')),
+            'port' => env('MYSQLPORT', env('DB_PORT', '3306')),
+            'database' => env('MYSQLDATABASE', env('DB_DATABASE', 'laravel')),
+            'username' => env('MYSQLUSER', env('DB_USERNAME', 'root')),
+            'password' => env('MYSQLPASSWORD', env('DB_PASSWORD', '')),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
